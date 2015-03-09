8c37f057e1be05f34d60740defded4895719e505
Alex Baines <alex@abaines.me.uk>
2015-03-09 15:03:59 +0000

    Better method of embedding the zip.

--- src/resource_system.cpp
+++ src/resource_system.cpp
@@ -3,30 +3,16 @@
 
 using namespace std;
 
-extern "C" {
-
-	asm (
-		".section .rodata\n"
-		".align 4\n"
-		".global _internal_zip_start\n"
-		"_internal_zip_start:\n"
-		".incbin \"internal.zip\"\n"
-		".global _internal_zip_end\n"
-		"_internal_zip_end:"
-	);
-
-	extern char _internal_zip_start[], _internal_zip_end[];
-
-}
+extern "C" char _binary_internal_zip_start[], _binary_internal_zip_end[];
 
 ResourceSystem::ResourceSystem(const char* argv0){
 	if(!PHYSFS_init(argv0)){
 		log(logging::error, "PhysFS init Error: %s", PHYSFS_getLastError());
 	}
 	
-	const size_t sz = _internal_zip_end - _internal_zip_start;
+	const size_t sz = _binary_internal_zip_end - _binary_internal_zip_start;
 
-	if(!PHYSFS_mountMemory(_internal_zip_start, sz, NULL, "internal.zip", NULL, 0)){
+	if(!PHYSFS_mountMemory(_binary_internal_zip_start, sz, NULL, ".zip", NULL, 0)){
 		log(logging::error, "PhysFS mount Error: %s", PHYSFS_getLastError());
 	}
 
